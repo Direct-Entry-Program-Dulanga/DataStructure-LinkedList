601a733827fc02f6ac04ed06995988a58928d329
Dulanga Nimesha <idulanganimesha@gmail.com>
2021-05-08 13:15:18 +0530

    initialize first commit

---
+++ src/Singly.java
@@ -0,0 +1,36 @@
+public class Singly {
+    public  void add(int number){
+
+    }
+
+    public void add(int index,int number){
+
+    }
+
+    public void remove(int index){
+
+    }
+
+    public int get(int undex){
+        return 0;
+    }
+
+    public void print(){
+
+    }
+    public void clear(){
+
+    }
+
+    public int size(){
+        return 0;
+    }
+
+    public boolean contains(int number){
+        return true;
+    }
+
+    public boolean empty(){
+        return false;
+    }
+}
